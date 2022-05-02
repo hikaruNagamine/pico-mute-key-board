a27c9eb210690c515a5fa6747135e5bde07a63a3
hikaruNagamine <hikaru.nagamine@gmail.com>
2022-05-02 13:38:29 +0900

    pull up key switch

--- src/main.rs
+++ src/main.rs
@@ -1,39 +1,32 @@
 #![no_std]
 #![no_main]
 
+use cortex_m::asm;
 use cortex_m_rt::entry;
 use defmt::*;
 use defmt_rtt as _;
 use embedded_hal::digital::v2::{InputPin, OutputPin};
-use embedded_time::fixed_point::FixedPoint;
+// use embedded_time::fixed_point::FixedPoint;
 use panic_probe as _;
 
 use bsp::hal;
 use hal::{
     clocks::{init_clocks_and_plls, Clock},
+    gpio::DynPin,
     pac,
     sio::Sio,
     usb::UsbBus,
     watchdog::Watchdog,
 };
-use pac::interrupt;
-use pac::interrupt::USBCTRL_IRQ;
 use rp_pico as bsp;
 
 use usb_device::class_prelude::UsbBusAllocator;
-use usb_device::prelude::{UsbDevice, UsbDeviceBuilder, UsbVidPid};
+use usb_device::prelude::{UsbDeviceBuilder, UsbVidPid};
 
-use usbd_hid::descriptor::KeyboardReport;
-use usbd_hid::descriptor::MouseReport;
-use usbd_hid::descriptor::SerializedDescriptor;
-use usbd_hid::hid_class::HIDClass;
-
-/// The USB Device Driver (shared with the interrupt).
-static mut USB_DEVICE: Option<UsbDevice<UsbBus>> = None;
-/// The USB Bus Driver (shared with the interrupt).
-static mut USB_BUS: Option<UsbBusAllocator<UsbBus>> = None;
-/// The USB Human Interface Device Driver (shared with the interrupt).
-static mut USB_HID: Option<HIDClass<UsbBus>> = None;
+use usbd_hid::{
+    descriptor::{KeyboardReport, SerializedDescriptor},
+    hid_class::HIDClass,
+};
 
 #[entry]
 fn main() -> ! {
@@ -63,35 +56,17 @@ fn main() -> ! {
         &mut pac.RESETS,
     );
     let bus_allocator = UsbBusAllocator::new(usb_bus);
-    unsafe {
-        // Note (safety): This is safe as interrupts haven't been started yet
-        USB_BUS = Some(bus_allocator);
-    }
 
-    let bus_ref = unsafe { USB_BUS.as_ref().unwrap() };
-    let usb_hid = HIDClass::new(bus_ref, MouseReport::desc(), 60);
-    unsafe {
-        // Note (safety): This is safe as interrupts haven't been started yet.
-        USB_HID = Some(usb_hid);
-    }
+    let mut usb_hid = HIDClass::new(&bus_allocator, KeyboardReport::desc(), 10);
 
     // Create a USB device with a fake VID and PID
     let vid_pid = UsbVidPid(0x16c0, 0x27da);
-    let usb_dev = UsbDeviceBuilder::new(bus_ref, vid_pid)
+    let mut usb_dev = UsbDeviceBuilder::new(&bus_allocator, vid_pid)
         .manufacturer("mitsumaru company")
         .product("pico-mute-key-board")
         .serial_number("TEST")
         .device_class(0x03) // misc
         .build();
-    unsafe {
-        // Note (safety): This is safe as interrupts haven't been started yet
-        USB_DEVICE = Some(usb_dev);
-    }
-
-    unsafe {
-        // Enable the USB interrupt
-        pac::NVIC::unmask(USBCTRL_IRQ);
-    };
 
     let sio = Sio::new(pac.SIO);
     let pins = bsp::Pins::new(
@@ -100,94 +75,150 @@ fn main() -> ! {
         sio.gpio_bank0,
         &mut pac.RESETS,
     );
-    let mut led_pin = pins.led.into_push_pull_output();
+    // let mut led_pin = pins.led.into_push_pull_output();
     // output
-    let mut row1 = pins.gpio2.into_push_pull_output();
-    // let row2 = pins.gpio4.into_push_pull_output();
-    // let row3 = pins.gpio3.into_push_pull_output();
+    let row1 = pins.gpio2.into_pull_up_input();
+    let row2 = pins.gpio4.into_pull_up_input();
+    let row3 = pins.gpio3.into_pull_up_input();
     // input
-    let col1 = pins.gpio19.into_pull_down_input();
-    // let col2 = pins.gpio18.into_pull_down_input();
+    let mut col1 = pins.gpio19.into_push_pull_output();
+    let mut col2 = pins.gpio18.into_push_pull_output();
 
-    let core = pac::CorePeripherals::take().unwrap();
-    let mut delay = cortex_m::delay::Delay::new(core.SYST, clocks.system_clock.freq().integer());
-    // let key_repo_a = KeyboardReport {
-    //     modifier: 0,
-    //     reserved: 0,
-    //     leds: 0,
-    //     keycodes: [0x04 /* A */, 0, 0, 0, 0, 0],
-    // };
-    // let key_repo_b = KeyboardReport {
-    //     modifier: 0,
-    //     reserved: 0,
-    //     leds: 0,
-    //     keycodes: [0x05 /* B */, 0, 0, 0, 0, 0],
-    // };
-    // let init_key_repo = KeyboardReport {
-    //     modifier: 0,
-    //     reserved: 0,
-    //     leds: 0,
-    //     keycodes: [0x00 /* A */, 0, 0, 0, 0, 0],
-    // };
+    // let rows: [&mut DynPin; 3] = [&mut row1.into(), &mut row2.into(), &mut row3.into()];
+    // let cols: [&mut DynPin; 2] = [&mut col1.into(), &mut col2.into()];
+
+    // let mut key_scanner = KeyScanner::new(rows, cols);
+    // let mut last_keycodes = [0u8; 6];
+
+    // let core = pac::CorePeripherals::take().unwrap();
+    // let mut delay = cortex_m::delay::Delay::new(core.SYST, clocks.system_clock.freq().integer());
+    let init_key_repo = KeyboardReport {
+        modifier: 0,
+        reserved: 0,
+        leds: 0,
+        keycodes: [0x00, 0, 0, 0, 0, 0],
+    };
     info!("Loop Start!");
-    row1.set_high().unwrap();
+    let mut count = 0;
+
+    col1.set_low().unwrap();
+    col2.set_high().unwrap();
+    asm::delay(1000);
 
     loop {
-        if col1.is_high().unwrap() {
-            led_pin.set_high().unwrap();
-            // push_key_event(key_repo_a).ok().unwrap_or(0);
-        } else {
-            led_pin.set_low().unwrap();
-            // push_key_event(init_key_repo).ok().unwrap_or(0);
+        // usb_dev.poll(&mut [&mut usb_hid]);
+        count += 1;
+        if row1.is_low().unwrap() {
+            info!("low!");
         }
-        // delay.delay_ms(100);
-
-        // let rep_up = MouseReport {
-        //     x: 0,
-        //     y: 4,
-        //     buttons: 0,
-        //     wheel: 0,
-        //     pan: 0,
-        // };
-        // push_mouse_movement(rep_up).ok().unwrap_or(0);
-
-        // delay.delay_ms(100);
-
-        // let rep_down = MouseReport {
-        //     x: 0,
-        //     y: -4,
-        //     buttons: 0,
-        //     wheel: 0,
-        //     pan: 0,
-        // };
-        // push_mouse_movement(rep_down).ok().unwrap_or(0);
+        if row2.is_low().unwrap() {
+            info!("low2!");
+        }
+        if row3.is_low().unwrap() {
+            info!("low3!");
+        }
+        // info!(
+        //     "loop {} : {},{},{},{},{},{}",
+        //     count,
+        //     last_keycodes[0],
+        //     last_keycodes[1],
+        //     last_keycodes[2],
+        //     last_keycodes[3],
+        //     last_keycodes[4],
+        //     last_keycodes[5]
+        // );
+        // let key_repo = key_scanner.scan();
+        // if last_keycodes != key_repo.keycodes {
+        //     debug!(
+        //         "change keycodes!!! {},{},{},{},{},{}",
+        //         key_repo.keycodes[0],
+        //         key_repo.keycodes[1],
+        //         key_repo.keycodes[2],
+        //         key_repo.keycodes[3],
+        //         key_repo.keycodes[4],
+        //         key_repo.keycodes[5]
+        //     );
+        //     // led_pin.set_high().unwrap();
+        //     usb_hid.push_input(&key_repo).ok();
+        //     last_keycodes = key_repo.keycodes;
+        // } else {
+        //     // led_pin.set_low().unwrap();
+        //     usb_hid.push_input(&init_key_repo).ok();
+        // }
+        // drop received data
+        // usb_hid.pull_raw_output(&mut [0; 64]).ok();
     }
 }
 
-/// Submit a new mouse movement report to the USB stack.
-///
-/// We do this with interrupts disabled, to avoid a race hazard with the USB IRQ.
-fn push_mouse_movement(report: MouseReport) -> Result<usize, usb_device::UsbError> {
-    cortex_m::interrupt::free(|_| unsafe {
-        // Now interrupts are disabled, grab the global variable and, if
-        // available, send it a HID report
-        USB_HID.as_mut().map(|hid| hid.push_input(&report))
-    })
-    .unwrap()
+const KEY_A: u8 = 0x04;
+const KEY_B: u8 = 0x05;
+const KEY_C: u8 = 0x06;
+const KEY_D: u8 = 0x07;
+const KEY_E: u8 = 0x08;
+const KEY_F: u8 = 0x09;
+#[allow(dead_code)]
+#[rustfmt::skip]
+const KEY_MAP: [[u8; 3]; 2] = [
+    [KEY_A, KEY_B, KEY_C], 
+    [KEY_D, KEY_E, KEY_F], 
+];
+struct KeyScanner<'a> {
+    rows: [&'a mut DynPin; 3],
+    cols: [&'a mut DynPin; 2],
 }
-pub fn push_key_event(report: KeyboardReport) -> Result<usize, usb_device::UsbError> {
-    // debug!("push_key_event");
-    cortex_m::interrupt::free(|_| unsafe { USB_HID.as_mut().map(|hid| hid.push_input(&report)) })
-        .unwrap()
+impl<'a> KeyScanner<'a> {
+    fn new(rows: [&'a mut DynPin; 3], cols: [&'a mut DynPin; 2]) -> Self {
+        KeyScanner { rows, cols }
+    }
+    fn scan_matrix(&mut self) -> [[bool; 3]; 2] {
+        let mut matrix = [[false; 3]; 2];
+        for (y, row) in matrix.iter_mut().enumerate() {
+            // info!("push!!! -- y {}", y);
+            for (i, pin_col) in self.cols.iter_mut().enumerate() {
+                if i == y {
+                    // info!("low!!! -- y {},{}", y, i);
+                    pin_col.set_low().unwrap();
+                } else {
+                    // info!("high!!! -- y {},{}", y, i);
+                    pin_col.set_high().unwrap();
+                }
+            }
+            asm::delay(1000); // Wait a bit to propagate the voltage
+            for (x, key) in row.iter_mut().enumerate() {
+                info!("y{},x{}", y, x);
+                *key = self.rows[x].is_low().unwrap();
+                if self.rows[x].is_low().unwrap() {
+                    info!("low!{}", x);
+                }
+            }
+        }
+        matrix
+    }
+    fn scan(&mut self) -> usbd_hid::descriptor::KeyboardReport {
+        // debug!("scan");
+        let mut key_codes = [0u8; 6];
+
+        let matrix = self.scan_matrix();
+        let mut next_keycode_index = 0;
+
+        for (y, row) in matrix.iter().enumerate() {
+            for (x, key) in row.iter().enumerate() {
+                if !*key {
+                    continue;
+                }
+                let key = KEY_MAP[y][x];
+                if next_keycode_index < key_codes.len() {
+                    info!("push! [{}][{}]", key, next_keycode_index);
+                    key_codes[next_keycode_index] = key;
+                    next_keycode_index += 1;
+                }
+            }
+        }
+        KeyboardReport {
+            modifier: 0,
+            reserved: 0,
+            leds: 0,
+            keycodes: key_codes,
+        }
+    }
 }
-/// This function is called whenever the USB Hardware generates an Interrupt
-/// Request.
-#[allow(non_snake_case)]
-#[interrupt]
-unsafe fn USBCTRL_IRQ() {
-    // Handle USB request
-    let usb_dev = USB_DEVICE.as_mut().unwrap();
-    let usb_hid = USB_HID.as_mut().unwrap();
-    usb_dev.poll(&mut [usb_hid]);
-}
-// End of file
